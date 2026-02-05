7042c99317ca5c44a140a597b9bec32f7d2a4a16
AlexisETB <alexistroya22@gmail.com>
2026-02-05 18:44:36 -0500

    feat: add route meta information and authorization guard for navigation

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requiereAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/about',
@@ -16,38 +20,78 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+      meta: {
+        requiereAutorizacion: false,
+        esPublica: true
+      }
     },
     {
       path: '/consultar-por-id',
       name: 'consultarPorId',
       component: () => import('../views/ConsultarPorIdView.vue'),
+      meta: {
+        requiereAutorizacion: false,
+        esPublica: true
+      }
     },
     {
       path: '/consultar-todos',
       name: 'consultarTodos',
       component: () => import('../views/ConsultarTodosView.vue'),
+      meta: {
+        requiereAutorizacion: false,
+        esPublica: true
+      }
     },
     {
       path: '/guardar',
       name: 'guardar',
       component: () => import('../views/GuardarView.vue'),
+      meta: {
+        requiereAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/actualizar',
       name: 'actualizar',
       component: () => import('../views/ActualizarView.vue'),
+      meta: {
+        requiereAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/actualizar-parcial',
       name: 'actualizarParcial',
       component: () => import('../views/ActualizarParcialView.vue'),
+      meta: {
+        requiereAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/borrar',
       name: 'borrar',
       component: () => import('../views/BorrarView.vue'),
+      meta: {
+        requiereAutorizacion: true,
+        esPublica: false
+      }
     }
   ],
+})
+
+/*Config del Guardian de Rutas*/
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiereAutorizacion) {
+    /*Envia a login*/
+    console.log("No autorizado. Redirigiendo a login...");
+  } else {
+    /*Deja pasar*/
+    console.log("Ruta pública. Deja pasar...");
+    next();
+  }
 })
 
 export default router
